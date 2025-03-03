2c62198b8d5ffe2fd6197313dad467f37a36d68b
Susan Lee <109441419+suleesan@users.noreply.github.com>
2025-03-03 02:53:52 -0800

    update timeline
    - includes hourly forecast for next 12 hours
    - includes 3 day forecast
    - set base for user prefs/recs to be inputted in when ready

--- app/timeline.tsx
+++ app/timeline.tsx
@@ -1,121 +1,206 @@
 import React, { useRef, useState, useEffect } from "react";
-import { View, Text, Image, Dimensions, ScrollView } from "react-native";
-import Slider from "@react-native-community/slider";
+import {
+  View,
+  Text,
+  Image,
+  Dimensions,
+  ScrollView,
+  ActivityIndicator,
+} from "react-native";
 import GradientBackground from "@/components/GradientBackground";
 import { AppText } from "@/components/AppText";
 
-const { width, height } = Dimensions.get("window");
+const { width } = Dimensions.get("window");
 
 function Timeline() {
-  const [sliderValue, setSliderValue] = useState(0);
+  const [weatherData, setWeatherData] = useState([]);
+  const [dailyForecast, setDailyForecast] = useState([]);
+  const [location, setLocation] = useState("");
+  const [loading, setLoading] = useState(true);
   const [gradientColors, setGradientColors] = useState([
-    "rgba(255, 222, 47, 0.26)",
-    "#B0E7F0",
-    "rgba(39, 91, 224, 0.3)",
+    "rgba(255, 183, 77, 0.8)",
+    "rgba(255, 244, 214, 0.9)",
+    "#4D92D9",
   ]);
   const scrollViewRef = useRef(null);
-  const groupCount = 3;
-  const timelineWidth = width * groupCount;
+
+  const API_KEY = "f076a815a1cbbdb3f228968604fdcc7a";
+  const CITY = "Palo Alto";
 
   const jacket = require("../assets/images/jacket.png");
   const pants = require("../assets/images/pants.png");
   const shirt = require("../assets/images/t-shirt.png");
   const umbrella = require("../assets/images/umbrella.png");
 
-  const iconGroups = [
-    { icons: [shirt, pants] },
-    { icons: [jacket, pants, umbrella] },
-    { icons: [jacket, pants, umbrella] },
-  ];
-
   useEffect(() => {
-    const groupIndex = Math.round(sliderValue * (groupCount - 1));
-    const scrollX = groupIndex * width;
+    const fetchWeather = async () => {
+      try {
+        const response = await fetch(
+          // api call is different from home; uses forecast not weather for 3 day forecast
+          `http://api.openweathermap.org/data/2.5/forecast?q=${CITY}&appid=${API_KEY}&units=imperial`
+        );
+        const data = await response.json();
+        setLocation(`${data.city.name}, ${data.city.country}`);
 
-    if (scrollViewRef.current) {
-      scrollViewRef.current.scrollTo({ x: scrollX, animated: true });
-    }
+        // hourly forecast for the next 12 hours
+        const hourlyForecast = data.list.slice(0, 5).map((hour) => ({
+          temp: hour.main.temp,
+          weather: hour.weather[0].main,
+          time: new Date(hour.dt * 1000).getHours(),
+        }));
 
-    if (groupIndex === 0) {
-      setGradientColors([
-        "rgba(255, 183, 77, 0.8)",
-        "rgba(255, 244, 214, 0.9)",
-        "#4D92D9",
-      ]);
-    } else if (groupIndex === 1) {
-      setGradientColors(["#4D92D9", "#4D92D9", "#4D92D9"]);
-    } else {
-      setGradientColors(["#4D92D9", "rgba(250, 159, 22, 0.8)", "#0B1A4"]);
-    }
-  }, [sliderValue]);
+        // 3 day forecast
+        const dailyForecastData = data.list
+          .filter((item) => item.dt_txt.includes("12:00:00")) // set for 12 PM each day
+          .slice(1, 4)
+          .map((day) => ({
+            date: day.dt_txt.split(" ")[0],
+            temp: day.main.temp,
+            weather: day.weather[0].main,
+          }));
+
+        setWeatherData(hourlyForecast);
+        setDailyForecast(dailyForecastData);
+        setLoading(false);
+      } catch (error) {
+        console.error("Error fetching weather:", error);
+        setLoading(false);
+      }
+    };
+
+    fetchWeather();
+  }, []);
+
+  // hardcoded for now; integrate with user's recommendations/prefs later
+  const getOutfitForWeather = (weather, temp) => {
+    if (weather.includes("Rain")) return [jacket, pants, umbrella];
+    if (temp < 50) return [jacket, pants];
+    if (temp >= 50 && temp < 70) return [shirt, pants];
+    return [shirt];
+  };
 
   return (
     <GradientBackground
       colors={gradientColors}
-      locations={[0.05, 0.5, 0.95]}
-      style={{ display: "flex", justifyContent: "center" }}
+      style={{ flex: 1, justifyContent: "center" }}
     >
-      <View
-        style={{
-          display: "flex",
-          justifyContent: "center",
-          alignItems: "center",
-          height: "60%",
-        }}
-      >
-        <ScrollView
-          style={styles.outfitContainer}
-          ref={scrollViewRef}
-          horizontal
-          scrollEnabled={false}
-          showsHorizontalScrollIndicator={false}
-          contentContainerStyle={{ width: timelineWidth }}
-        >
-          {iconGroups.map((group, index) => (
-            <View
-              key={index}
-              style={[styles.iconGroup, { width, justifyContent: "center" }]}
+      <View style={{ flex: 1, marginTop: 50 }}>
+        {loading ? (
+          <ActivityIndicator size="large" color="#FFFFFF" />
+        ) : (
+          <>
+            <AppText
+              type="subtitle"
+              style={{
+                fontSize: 30,
+                textAlign: "center",
+                marginVertical: 20,
+              }}
             >
-              {group.icons.map((icon, idx) => (
-                <Image key={idx} source={icon} style={styles.icon} />
+              {/* ignore country */}
+              {location.split(",")[0]} Timeline
+            </AppText>
+
+            <AppText
+              type="defaultSemiBold"
+              style={{ marginBottom: 10, marginLeft: 10 }}
+            >
+              Next 12 Hours
+            </AppText>
+            <ScrollView
+              ref={scrollViewRef}
+              horizontal
+              showsHorizontalScrollIndicator={true}
+              contentContainerStyle={{
+                width: ((2 * width) / 3) * weatherData.length,
+                height: 200,
+                backgroundColor: "rgba(255, 255, 255, 0.3)",
+                padding: 10,
+              }}
+            >
+              {weatherData.map((hour, index) => (
+                <View
+                  key={index}
+                  style={{
+                    width: (2 * width) / 3,
+                    justifyContent: "center",
+                    alignItems: "center",
+                  }}
+                >
+                  <AppText>{hour.time}:00</AppText>
+                  <AppText>
+                    {hour.temp}°F - {hour.weather}
+                  </AppText>
+                  <View
+                    style={{ flexDirection: "row", justifyContent: "center" }}
+                  >
+                    {getOutfitForWeather(hour.weather, hour.temp).map(
+                      (icon, idx) => (
+                        <Image
+                          key={idx}
+                          source={icon}
+                          style={{ height: 80, width: 80, margin: 5 }}
+                        />
+                      )
+                    )}
+                  </View>
+                </View>
               ))}
-            </View>
-          ))}
-        </ScrollView>
-        <Slider
-          style={styles.slider}
-          minimumValue={0}
-          maximumValue={1}
-          step={1 / (groupCount - 1)}
-          minimumTrackTintColor="#FFFFFF"
-          maximumTrackTintColor="#808080"
-          value={sliderValue}
-          onValueChange={setSliderValue}
-        />
-        <View style={styles.timeLabelsContainer}>
-          <AppText style={styles.timeLabel}>9 AM</AppText>
-          <AppText style={styles.timeLabel}>12 PM</AppText>
-          <AppText style={styles.timeLabel}>9 PM</AppText>
-        </View>
+            </ScrollView>
+            <AppText
+              type="defaultSemiBold"
+              style={{ marginVertical: 20, marginLeft: 10 }}
+            >
+              Next 3 Days
+            </AppText>
+            <ScrollView
+              contentContainerStyle={{
+                alignItems: "center",
+              }}
+            >
+              {dailyForecast.map((day, index) => (
+                <View
+                  key={index}
+                  style={{
+                    width: width * 0.9,
+                    backgroundColor: "rgba(255,255,255,0.3)",
+                    padding: 15,
+                    marginBottom: 10,
+                    borderRadius: 10,
+                  }}
+                >
+                  <AppText>{day.date}</AppText>
+                  <AppText>
+                    {day.temp}°F - {day.weather}
+                  </AppText>
+                  <View
+                    style={{ flexDirection: "row", justifyContent: "center" }}
+                  >
+                    {getOutfitForWeather(day.weather, day.temp).map(
+                      (icon, idx) => (
+                        <Image
+                          key={idx}
+                          source={icon}
+                          style={{ height: 40, width: 40, margin: 10 }}
+                        />
+                      )
+                    )}
+                  </View>
+                </View>
+              ))}
+            </ScrollView>
+          </>
+        )}
       </View>
     </GradientBackground>
   );
 }
 
+export default Timeline;
+
 import { StyleSheet } from "react-native";
 
 const styles = StyleSheet.create({
-  outfitContainer: {
-    width: "100%",
-    backgroundColor: "rgba(255, 255, 255, 0.5)",
-    borderRadius: 25,
-    padding: 10,
-  },
-  slider: {
-    width: width * 0.9,
-    height: 20,
-    marginTop: 20,
-  },
   iconGroup: {
     flexDirection: "column",
     alignItems: "center",
@@ -139,5 +224,3 @@ const styles = StyleSheet.create({
     fontWeight: "bold",
   },
 });
-
-export default Timeline;
